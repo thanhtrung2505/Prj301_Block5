4a88738f6774fd3fece0e1d9bd50466361be0862
thanhtrung2505 <66957755+thanhtrung2505@users.noreply.github.com>
2021-12-07 01:23:46 +0700

    dal,student

---
+++ src/java/model/Student.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Do Phong PC
+ */
+public class Student {
+    public int stuID;
+    public String stuName;
+    public String stuCode;
+    public int stuGroup;
+
+    public int getStuID() {
+        return stuID;
+    }
+
+    public void setStuID(int stuID) {
+        this.stuID = stuID;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getStuCode() {
+        return stuCode;
+    }
+
+    public void setStuCode(String stuCode) {
+        this.stuCode = stuCode;
+    }
+
+    public int getStuGroup() {
+        return stuGroup;
+    }
+
+    public void setStuGroup(int stuGroup) {
+        this.stuGroup = stuGroup;
+    }
+    
+}
